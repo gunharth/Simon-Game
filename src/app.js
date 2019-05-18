6baafb820bdc564c5d34c8574aa2df010cf36903
Gunharth Randolf <hello@gunharth.io>
2019-05-18 15:02:48 +0200

    Add loading animation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import { SimonGame } from './simon-game.js';
+import { timeout } from 'q';
 
 customElements.define('simon-game', SimonGame);
+
+// loading animation
+const loader = document.getElementById('loader');
+setTimeout(() => {
+    loader.firstChild.classList.add('rotating');
+}, 700);
+setTimeout(() => {
+    loader.classList.add('is-hidden');
+}, 1400);
+setTimeout(() => {
+    loader.remove();
+}, 3400);
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
